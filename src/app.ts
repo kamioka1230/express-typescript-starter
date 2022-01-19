c9b4a76b23286c1f2aa2572e20ff3872d440fbbd
kamioka1230 <hiphop.pizzaboi@gmail.com>
2022-01-19 14:41:58 +0900

    add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import createError from 'http-errors';
+import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 
 import indexRouter from './routes/index';
@@ -14,6 +15,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // router setup
 app.use('/', indexRouter());
+
+// error test router
+app.use('/404', function(req, res, next) {
+    next(createError(404));
+});
+app.use('/500', function(req, res, next) {
+    next(createError(500));
+});
+
+// error handling
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 
 const port = '3000';
 app.set('port', port);
